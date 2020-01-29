08149a7575f731f4aafef3dcbc8b021fd04dc4a4
HelloWorldwangjiacheng <280167437@qq.com>
2020-01-29 22:01:51 +0800

    将图片存储到ucloud的uFile里面

--- src/main/java/life/majiang/community/community/controller/FileUploadController.java
+++ src/main/java/life/majiang/community/community/controller/FileUploadController.java
@@ -1,15 +1,33 @@
 package life.majiang.community.community.controller;
 
 import life.majiang.community.community.data_transfer_model.FileUploadDTO;
+import life.majiang.community.community.provider.UCloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Controller
 public class FileUploadController {
+
+    @Autowired
+    private UCloudProvider uCloudProvider;
+
     @RequestMapping("/file/upload")
     @ResponseBody
-    public FileUploadDTO upload(){
+    public FileUploadDTO upload(HttpServletRequest request){
+        MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartHttpServletRequest.getFile("editormd-image-file");
+        try {
+            uCloudProvider.upload(file.getInputStream(),file.getContentType(),file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         FileUploadDTO fileUploadDTO = new FileUploadDTO();
         fileUploadDTO.setSuccess(1);
         fileUploadDTO.setUrl("/images/gala.jpg");
